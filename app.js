03256f35ffa545d5e3b25e121e6c8093d446e61b
sohail8247 <sohail82474@gmail.com>
2024-09-12 19:15:14 +0530

    sohail

--- app.js
+++ app.js
@@ -7,11 +7,15 @@
 //console.log(parent);
   //  const root=ReactDOM.createRoot(document.getElementById("root"));
     //root.render(parent);
-const parent=React.createElement("div",{id:"parent"},[
-    React.createElement("div",{id:"child"},
-    React.createElement("h1",{},"assalamualikum"),
-    React.createElement("h2",{},"assalamualikum"),
-)]);
-    console.log(parent);
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+    
+    //import react from "react";
+    //import React-dom from "React-dOM";
+//const parent=React.createElement("div",{id:"parent"},[
+  //  React.createElement("div",{id:"child"},
+    //React.createElement("h1",{},"assalamualikum"),
+    //React.createElement("h2",{},"assalamualikum"),
+//)]);
+    //console.log(parent);
+    //const root=ReactDOM.createRoot(document.getElementById("root"));
+    //root.render(parent);
+console.log("sohail");
